42ed27cefc941d89c0f424b5b7c43f718010f5db
cristian <cristrd3@gmail.com>
2023-01-25 16:45:47 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-/*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-   
+
    window.addEventListener('DOMContentLoaded', event => {
     // Función de reducción de la barra de navegación
     var navbarShrink = function () {
